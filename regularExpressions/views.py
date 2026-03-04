e42a193ba9c1cf2b36174ab852303ac41ded0629
drin74 <bsoanarman@gmail.com>
2026-03-04 20:23:40 +0300

    add get file2, delete error file not txt

--- regularExpressions/views.py
+++ regularExpressions/views.py
@@ -6,19 +6,24 @@ def index(request):
     test_string = ""
     matches = []
 
-
     if request.method == "POST":
         regex = request.POST.get("regexInput", "")
 
-        # Проверяем, был ли загружен файл
         if request.FILES.get("fileInput"):
             uploaded_file = request.FILES["fileInput"]
-            # Читаем содержимое файла
-            test_string = uploaded_file.read().decode('utf-8')
+
+            # Новая проверка!!!
+            if not uploaded_file.name.endswith('.txt'):
+                matches = ["Ошибка: Можно загружать только файлы .txt!"]
+                test_string = request.POST.get("testString", "")
+            else:
+                test_string = uploaded_file.read().decode('utf-8')
         else:
             test_string = request.POST.get("testString", "")
+
         try:
-            matches = re.findall(regex, test_string)
+            if not matches:  # Ищем только если нет ошибки
+                matches = re.findall(regex, test_string)
         except re.error:
             matches = ["Ошибка в регулярном выражении!"]
 
